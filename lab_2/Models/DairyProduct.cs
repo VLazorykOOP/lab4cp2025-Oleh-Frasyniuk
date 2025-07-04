2ef82d2b9d093fe9f47fb6b5c37b1379565f920b
Oleh-Frasyniuk <158566038+Oleh-Frasyniuk@users.noreply.github.com>
2025-07-04 15:02:37 +0100

    Add files via upload

---
+++ lab_2/Models/DairyProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4.Models
+{
+    public class DairyProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }         
+        public string Category { get; set; }     
+        public string Type { get; set; }         
+        public DateTime ExpirationDate { get; set; } 
+        public string Supplier { get; set; }    
+        public decimal Price { get; set; }       
+    }
+}
---
+++ lab_2/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } 
+    }
+}
